065f5da674a673024898c695774f92ccd6b82516
josiah <josiahdamiwilliams@gmail.com>
2020-03-25 15:54:59 +0100

    suceed and repair tests working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,20 +2,37 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+//  
+if(item.enhancement < 20 ){
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement + 1
+  }
+  return newItem;
+}else {
+  return item;
 }
 
-function fail(item) {
-  return { ...item };
+// function fail(item) {
+//   return { ...item };
+// }
 }
-
 function repair(item) {
-  return { ...item };
+  
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
+
+
 
 function get(item) {
   return { ...item };
